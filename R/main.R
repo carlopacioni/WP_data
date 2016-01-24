d3184ca3b0452b60e4d27b118ee95c3b17208f77
Carlo Pacioni <carlo.pacioni@gmail.com>
2016-01-24 19:12:27 +0800

    trap.year

--- R/main.R
+++ R/main.R
@@ -102,3 +102,23 @@ read.trapping <- function(dir.in=NULL,
   return(data)
 }
 
+#' Trapping across years
+#'
+#' \code{trap.year} calculates the number of sessions within each year, for each
+#' species.
+#'
+#' @param data The input data (output from \code{read.trapping})
+#' @import data.table
+#' @export
+trap.year <- function(data) {
+  ntsession <- function(yr, V1) length(V1[format(V1, "%Y") == yr])
+
+  dt <- data.table(data)
+  dtuni <- dt[, unique(Date), by="Species"]
+
+  yrs<-sort(unique(format(dt[, unique(Date)][!is.na(d)], format="%Y")))
+
+  tryrs <- dtuni[, lapply(yrs, ntsession, V1), by="Species"]
+  setnames(tryrs, c("Species", yrs))
+  return(tryrs)
+}
